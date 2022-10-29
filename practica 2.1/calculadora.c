fddb8fd60b918cc511063c654d26f18f53a957a8
ulises rivera garcia <jonhgmb28>
2022-10-28 23:59:15 -0500

    ya

--- practica 2.1/calculadora.c
+++ practica 2.1/calculadora.c
@@ -20,9 +20,7 @@ float calculo(float num1, float num2, char operador){
         resultado = num1 / num2;
         break;
     case '%':
-         num1 = (int)num1;
-         num2 = (int)num2;
-        resultado = num1 % num2;
+        resultado = (int)num1 % (int)num2;
         break;
     default:
         printf("Operacion no valida");
@@ -34,7 +32,7 @@ float calculo(float num1, float num2, char operador){
 int main(){
 
     printf("Bienvenido a la calculadora, tu operacion de forma n+n siendo (+) el operador deseado y n los numeros a operar\n");
-    scanf("%.2f%.2c%f", &num1 ,&operdador, &num2);
+    scanf("%f%c%f", &num1 ,&operdador, &num2);
     calculo(num1, num2, operdador);
     printf("El resultado es: %.2f", resultado);
 
