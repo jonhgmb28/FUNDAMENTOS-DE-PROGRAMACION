de5cdb449b7d1ade99366c6f6ea922e723e19709
ulises rivera garcia <jonhgmb28>
2022-11-26 01:24:41 -0600

    finish

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,30 +3,29 @@
 void ejercicio(){
 
     int num;
+    int factorial=1;
 
     do{
         printf("Ingrese un numero mayor a 0: ");
         scanf("%d", &num);
         
     } while (num < 0);
-
-    int suma;//esta variable es para guardar el factorial, ya que como incializo la variable factorial en cada iteracion del for en 1, los datos se pierden
+    
+    int suma=0;//esta variable es para guardar el factorial, ya que como incializo la variable factorial en cada iteracion del for en 1, los datos se pierden
     
     for (int i = 1; i <= num; i++){
-        int factorial=1;
+        int fac = 1;
         printf("%d! + ", i);
-        for (int j = 1; j <= i; j++)
-        {
-            
-            factorial = factorial * j;
-            suma=factorial;
-            
-
-            
+        for (int j = 1; j <= i; j++){
+           
+           fac = fac * j;
+             
         }
-        
+        suma=fac+suma;
+    
     }
     printf(" = %d", suma);
+    
 
 }
 
