3e751402c1f0084e86303a8228a24c83414c8909
ulises rivera garcia <jonhgmb28>
2022-11-26 23:49:28 -0600

    oowa

--- practica 3.1/ejercicio1.c
+++ practica 3.1/ejercicio1.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
-void validar(){
+void ejercicio(){
     int num;
+
     do{
         printf("Ingrese un numero mayor a 0: ");
         scanf("%d", &num);
-    } while (num < 0);
-    
-   
-    
+        
+    } while (num <= 0);
+
+    int contador = num;
+
+    for (int i = 1; i <= contador; i++){
+        for (int j = 1; j <= num; j++){
+        
+        printf("%d", j);
+        
+        }
+        printf("\n");
+        num = num-1;
+    }
+
 }
 
 int main(){
 
-    validar();
+    ejercicio();
 
     return 0;
 }
